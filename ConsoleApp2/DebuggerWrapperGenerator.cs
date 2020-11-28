a81acf719fe9faad331ab203cc633f5bfaf621c0
tkhadimullin <bresspm@gmail.com>
2020-11-28 23:26:08 +1300

    style updates

--- ConsoleApp2/DebuggerWrapperGenerator.cs
+++ ConsoleApp2/DebuggerWrapperGenerator.cs
@@ -5,35 +5,35 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp2
 {
-public static class DebuggerWrapperGenerator
-{
-    public static object CreateWrapper(Type ServiceType, MethodInfo baseMethod)
+    public static class DebuggerWrapperGenerator
     {
-        var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName($"newAssembly_{Guid.NewGuid()}"), AssemblyBuilderAccess.Run);
-        var module = asmBuilder.DefineDynamicModule($"DynamicAssembly_{Guid.NewGuid()}");
-        var typeBuilder = module.DefineType($"DynamicType_{Guid.NewGuid()}", TypeAttributes.Public, ServiceType);
-        var methodBuilder = typeBuilder.DefineMethod("Run", MethodAttributes.Public | MethodAttributes.NewSlot);
+        public static object CreateWrapper(Type ServiceType, MethodInfo baseMethod)
+        {
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName($"newAssembly_{Guid.NewGuid()}"), AssemblyBuilderAccess.Run);
+            var module = asmBuilder.DefineDynamicModule($"DynamicAssembly_{Guid.NewGuid()}");
+            var typeBuilder = module.DefineType($"DynamicType_{Guid.NewGuid()}", TypeAttributes.Public, ServiceType);
+            var methodBuilder = typeBuilder.DefineMethod("Run", MethodAttributes.Public | MethodAttributes.NewSlot);
 
-        var ilGenerator = methodBuilder.GetILGenerator();
+            var ilGenerator = methodBuilder.GetILGenerator();
 
-        ilGenerator.EmitCall(OpCodes.Call, typeof(Debugger).GetMethod("Launch", BindingFlags.Static | BindingFlags.Public), null);
-        ilGenerator.Emit(OpCodes.Pop);
+            ilGenerator.EmitCall(OpCodes.Call, typeof(Debugger).GetMethod("Launch", BindingFlags.Static | BindingFlags.Public), null);
+            ilGenerator.Emit(OpCodes.Pop);
 
-        ilGenerator.Emit(OpCodes.Ldarg_0);
-        ilGenerator.EmitCall(OpCodes.Call, baseMethod, null);
-        ilGenerator.Emit(OpCodes.Ret);
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.EmitCall(OpCodes.Call, baseMethod, null);
+            ilGenerator.Emit(OpCodes.Ret);
 
-        /*
-         * the generated method would be roughly equivalent to:
-         * new void Run()
-         * {
-         *   Debugger.Launch();
-         *   base.Run();
-         * }
-         */
+            /*
+             * the generated method would be roughly equivalent to:
+             * new void Run()
+             * {
+             *   Debugger.Launch();
+             *   base.Run();
+             * }
+             */
 
-        var wrapperType = typeBuilder.CreateType();
-        return Activator.CreateInstance(wrapperType);
+            var wrapperType = typeBuilder.CreateType();
+            return Activator.CreateInstance(wrapperType);
+        }
     }
-}
 }
--- ConsoleApp2/Launcher.cs
+++ ConsoleApp2/Launcher.cs
@@ -6,30 +6,30 @@ using System.Reflection.Emit;
 
 namespace ConsoleApp2
 {
-public class Launcher : MarshalByRefObject
-{
-    private Type ServiceType { get; }
-
-    public Launcher(string pathToDll)
+    public class Launcher : MarshalByRefObject
     {
-        var assembly = Assembly.LoadFrom(pathToDll);
-        ServiceType = assembly.GetTypes().SingleOrDefault(t => t.Name == "Class1");
+        private Type ServiceType { get; }
+
+        public Launcher(string pathToDll)
+        {
+            var assembly = Assembly.LoadFrom(pathToDll);
+            ServiceType = assembly.GetTypes().SingleOrDefault(t => t.Name == "Class1");
+        }
+
+        public void Run()
+        {
+            var wrappedInstance = DebuggerWrapperGenerator.CreateWrapper(ServiceType, ServiceType.GetMethod("Run"));
+            wrappedInstance.GetType().GetMethod("Run")?.Invoke(wrappedInstance, null);
+        }
+
+        public static void RunInNewAppDomain(string pathToDll)
+        {
+            var appDomain = AppDomain.CreateDomain("AppDomainInLauncher", AppDomain.CurrentDomain.Evidence, AppDomain.CurrentDomain.SetupInformation);
+
+            var launcher = appDomain.CreateInstanceAndUnwrap(typeof(Launcher).Assembly.FullName, typeof(Launcher).FullName, false, BindingFlags.Public | BindingFlags.Instance,
+                null, new object[] { pathToDll }, CultureInfo.CurrentCulture, null);
+            (launcher as Launcher)?.Run();
+
+        }
     }
-
-    public void Run()
-    {
-        var wrappedInstance = DebuggerWrapperGenerator.CreateWrapper(ServiceType, ServiceType.GetMethod("Run"));
-        wrappedInstance.GetType().GetMethod("Run")?.Invoke(wrappedInstance, null);
-    }
-
-    public static void RunInNewAppDomain(string pathToDll)
-    {
-        var appDomain = AppDomain.CreateDomain("AppDomainInLauncher", AppDomain.CurrentDomain.Evidence, AppDomain.CurrentDomain.SetupInformation);
-
-        var launcher = appDomain.CreateInstanceAndUnwrap(typeof(Launcher).Assembly.FullName, typeof(Launcher).FullName, false, BindingFlags.Public|BindingFlags.Instance,
-            null, new object[] { pathToDll }, CultureInfo.CurrentCulture, null);
-        (launcher as Launcher)?.Run();
-
-    }
-}
 }
